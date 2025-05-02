2039d10ce5236aa2db433e53a5c1d3033fd88c5f
qs-crocodyle <showhorn@gmail.com>
2025-05-02 08:29:16 -0500

    Update database to latest

--- ItemLevel.lua
+++ ItemLevel.lua
@@ -1,253 +1,387 @@
-ItemLevel = CreateFrame( "Frame" , "ItemLevelTooltip", GameTooltip )
+--Modified from S_ItemTip, a Chinese addon, 20250502
+
+local EquipSoltList = {
+	INVTYPE_2HWEAPON,
+	-- INVTYPE_BODY,
+	INVTYPE_CHEST,
+	INVTYPE_CLOAK,
+	INVTYPE_FEET,
+	INVTYPE_FINGER,
+	INVTYPE_HAND,
+	INVTYPE_HEAD,
+	INVTYPE_HOLDABLE,
+	INVTYPE_LEGS,
+	INVTYPE_NECK,
+	INVTYPE_RANGED,
+	INVTYPE_RELIC,
+	INVTYPE_ROBE,
+	INVTYPE_SHIELD,
+	INVTYPE_SHOULDER,
+	-- INVTYPE_TABARD,
+	INVTYPE_TRINKET,
+	INVTYPE_WAIST,
+	INVTYPE_WEAPON,
+	INVTYPE_WEAPONMAINHAND,
+	INVTYPE_WEAPONOFFHAND,
+	INVTYPE_WRIST,
+	"Gun",
+	"Crossbow",
+	"Wand",
+	"Thrown",
+}
+
+local function IsEquip()
+	if ItemRefTooltip:IsVisible() then
+		for i = 2,5 do
+			local EquipText = getglobal("ItemRefTooltipTextLeft"..i):GetText()
+			for _, equip in pairs(EquipSoltList) do
+				if EquipText == equip then
+					return true
+				end
+			end
+		end
+	else
+		for i = 2,5 do
+			local EquipText = getglobal("GameTooltipTextLeft"..i):GetText()
+			for _, equip in pairs(EquipSoltList) do
+				if EquipText == equip then
+					return true
+				end
+			end
+		end	
+	end
+	
+	return false
+end
+
+local function Add_ItemLevelLine(itemID, itemLevel)
+	if not itemID then return end
+	
+	if IsEquip() and itemLevel and itemLevel > 0 then
+		if ItemRefTooltip:IsVisible() then
+			ItemRefTooltip:Show()
+		else
+			GameTooltip:AddLine("ItemLevel: " .. itemLevel, 1, 1, 0)
+			GameTooltip:Show()
+		end
+	end
+end
+
+ItemLevel = CreateFrame("Frame" , "ItemLevelTooltip", GameTooltip)
 ItemLevel:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
 ItemLevel:SetScript("OnEvent", function()
-  ilvl, r, g, b = ItemLevel:ScanUnit("mouseover")
-  if ilvl and r and g and b then
-    GameTooltip:AddLine("ItemLevel: " .. ilvl, r,g,b)
-    GameTooltip:Show()
-  end
+	local ilvl = this:ScanUnit("mouseover")
+	if ilvl and ilvl > 0 then
+		GameTooltip:AddLine("ItemLevel: " .. ilvl, 1, 1, 0)
+		GameTooltip:Show()
+	end
 end)
 
 ItemLevel:SetScript("OnShow", function()
-  if GameTooltip.itemLink then
-    local _, _, itemID = string.find(GameTooltip.itemLink, "item:(%d+):%d+:%d+:%d+")
-    local _, _, itemLink = string.find(GameTooltip.itemLink, "(item:%d+:%d+:%d+:%d+)");
-
-    if not itemLink then return end
-
-    local ilvl = ItemLevel.Database[tonumber(itemID)] or 0
-    local _, _, itemRarity, _, _, _, _, itemSlot, _ = GetItemInfo(itemLink)
-    local r,g,b = GetItemQualityColor(itemRarity)
-
-    if ilvl and ilvl > 0 then
-      GameTooltip:AddLine("ItemLevel: " .. ilvl, r, g, b)
-      GameTooltip:Show()
-    end
-  end
+	if GameTooltip.itemLink then
+		local _, _, itemID = string.find(GameTooltip.itemLink, "item:(%d+)")
+		local itemLevel = ItemLevel.ilvl_database[tonumber(itemID)] or 0
+		Add_ItemLevelLine(itemID, itemLevel)
+	end
 end)
 
 ItemLevel:SetScript("OnHide", function()
-  GameTooltip.itemLink = nil
+	GameTooltip.itemLink = nil
 end)
 
-local function GetItemLinkByName(name)
-  for itemID = 1, 65536 do
-    local itemName, hyperLink, itemQuality = GetItemInfo(itemID)
-    if (itemName and itemName == name) then
-      local _, _, _, hex = GetItemQualityColor(tonumber(itemQuality))
-      return hex.. "|H"..hyperLink.."|h["..itemName.."]|h|r"
-    end
-  end
+hooksecurefunc("SetItemRef", function(link, button)
+	if ItemRefTooltip:IsVisible() then
+		local _, _, itemID = string.find(link, "item:(%d+)")
+		local itemLevel = ItemLevel.ilvl_database[tonumber(itemID)] or 0
+		Add_ItemLevelLine(itemID, itemLevel)
+	end
+end)
+
+if IsAddOnLoaded("StatCompare") then
+	StatCompareSelfFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+	StatCompareSelfFrame:RegisterEvent("UNIT_INVENTORY_CHANGED")
+	StatCompareSelfFrame:RegisterEvent("BAG_UPDATE")
+	StatCompareSelfFrame:SetScript("OnEvent", function()
+		local ilvl = ItemLevel:ScanUnit("player")
+		if ilvl and ilvl > 0 then
+			PlayerItemGS:SetText("ItemLevel: " .. ilvl)
+			PlayerItemGS:SetTextColor(1, 1, 0)
+		else
+			PlayerItemGS:SetText()
+		end
+	end)
 end
 
--- target inspect
+-- target inspect frame
 ItemLevelHookInspectUnit = InspectUnit
 function InspectUnit(unit)
-  ItemLevelHookInspectUnit(unit)
-  ItemLevel.Inspect = ItemLevel.Inspect or CreateFrame("Frame", nil, InspectModelFrame)
-  ItemLevel.Inspect:SetFrameStrata("HIGH")
-  ItemLevel.Inspect:SetWidth(200)
-  ItemLevel.Inspect:SetHeight(25)
-  ItemLevel.Inspect:SetPoint("BOTTOM", 0, 0)
-  ItemLevel.Inspect.text = ItemLevel.Inspect.text or ItemLevel.Inspect:CreateFontString("Status", "TOOLTIP", "GameFontNormal")
-  ItemLevel.Inspect.text:SetPoint("CENTER", 0, 0)
+    ItemLevelHookInspectUnit(unit)
+    ItemLevel.Inspect = ItemLevel.Inspect or CreateFrame("Frame", nil, InspectModelFrame)
+    ItemLevel.Inspect:SetFrameStrata("HIGH")
+    ItemLevel.Inspect:SetWidth(200)
+    ItemLevel.Inspect:SetHeight(25)
+    ItemLevel.Inspect:SetPoint("BOTTOM", 0, 0)
+    ItemLevel.Inspect.text = ItemLevel.Inspect.text or ItemLevel.Inspect:CreateFontString("Status", "TOOLTIP", "GameFontNormal")
+    ItemLevel.Inspect.text:SetPoint("CENTER", 0, 0)
 
-  local ilvl, r, g, b = ItemLevel:ScanUnit("target")
-  if ilvl and r and g and b then
-    ItemLevel.Inspect.text:SetText("ItemLevel: " .. ilvl)
-    ItemLevel.Inspect.text:SetTextColor(r, g, b)
-  end
+    local ilvl = ItemLevel:ScanUnit("target")
+    if ilvl and ilvl > 0 then
+        ItemLevel.Inspect.text:SetText("ItemLevel: " .. ilvl)
+        ItemLevel.Inspect.text:SetTextColor(1, 1, 0)
+    end
+	if IsAddOnLoaded("StatCompare") then
+		if ilvl and ilvl > 0 then
+			TargetItemGS:SetText("ItemLevel: " .. ilvl)
+			TargetItemGS:SetTextColor(1, 1, 0)
+		else
+			TargetItemGS:SetText()
+		end
+	end
 end
 
--- player inspect
+-- -- player inspect frame
 ItemLevel.CharacterFrame = CreateFrame("Frame", nil, CharacterModelFrame)
 ItemLevel.CharacterFrame:SetFrameStrata("HIGH")
 ItemLevel.CharacterFrame:SetWidth(200)
 ItemLevel.CharacterFrame:SetHeight(50)
 ItemLevel.CharacterFrame:SetPoint("BOTTOM", 0, 0)
 ItemLevel.CharacterFrame.text = ItemLevel.CharacterFrame:CreateFontString("Status", "LOW", "GameFontNormal")
+ItemLevel.CharacterFrame.text:SetPoint("CENTER", 0, 0)
 
 ItemLevel.CharacterFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
 ItemLevel.CharacterFrame:RegisterEvent("UNIT_NAME_UPDATE")
 ItemLevel.CharacterFrame:RegisterEvent("UNIT_PORTRAIT_UPDATE")
 ItemLevel.CharacterFrame:RegisterEvent("BAG_UPDATE")
 ItemLevel.CharacterFrame:SetScript("OnEvent", function()
-  ilvl, r, g, b = ItemLevel:ScanUnit("player")
-  if ilvl and r and g and b then
-    ItemLevel.CharacterFrame.text:SetText("ItemLevel: " .. ilvl)
-    ItemLevel.CharacterFrame.text:SetTextColor(r, g, b)
-  end
+    ilvl = ItemLevel:ScanUnit("player")
+    if ilvl and ilvl > 0 then
+        ItemLevel.CharacterFrame.text:SetText("ItemLevel: " .. ilvl)
+        ItemLevel.CharacterFrame.text:SetTextColor(1, 1, 0)
+    end
 end)
 
---- BetterCharacterStats compatibility check
-if BCSFrame then
-  ItemLevel.CharacterFrame.text:SetPoint("CENTER", 0, 20)
-else
-  ItemLevel.CharacterFrame.text:SetPoint("CENTER", 0, 0)
+function ItemLevel:Calculate(rarity, ilvl)
+	if not rarity then rarity = 0 end
+	
+	local qualityScale = 1
+	if rarity == 5 then
+		qualityScale = 1.3
+	elseif rarity == 4 then
+		qualityScale = 1.0
+	elseif rarity == 3 then
+		qualityScale = 0.85
+	elseif rarity == 2 then
+		qualityScale = 0.6
+	elseif rarity == 1 then
+		qualityScale = 0.25
+	end
+	
+	return ilvl * qualityScale
 end
 
--- functions
-function ItemLevel:round(input, places)
-  if not places then places = 0 end
-  if type(input) == "number" and type(places) == "number" then
-    local pow = 1
-    for i = 1, places do pow = pow * 10 end
-    return floor(input * pow + 0.5) / pow
-  end
-end
+function ItemLevel:ScanUnit(unit)
+    if not UnitIsPlayer(unit) then return nil end
 
-function ItemLevel:ScanUnit(target)
-  if not UnitIsPlayer(target) then return nil end
+    local ilvl = 0
+    local _, class = UnitClass(unit)
+    local isHunter = (class == "HUNTER")
+    local hasMainHand = (GetInventoryItemLink(unit, 16) ~= nil)
+    local hasOffhand = (GetInventoryItemLink(unit, 17) ~= nil)
 
-  local count, ar, ag, ab, ilvl, icount = 0, 0, 0, 0, 0, 0
+    local slotCoefficients = {
+        [1] = 1,
+        [2] = 0.5625,
+        [3] = 0.75,
+        [5] = 1,
+        [6] = 0.5625,
+        [7] = 0.75,
+        [8] = 0.75,
+        [9] = 0.75,
+        [10] = 0.75,
+        [11] = 0.5625,
+        [12] = 0.5625,
+        [13] = 0.5625,
+        [14] = 0.5625,
+        [15] = 0.5625,
+        [16] = 1,
+        [17] = 1,
+        [18] = 0.3164,
+    }
+	-- 0 = ammo
+	-- 1 = head
+	-- 2 = neck
+	-- 3 = shoulder
+	-- 4 = shirt
+	-- 5 = chest
+	-- 6 = waist
+	-- 7 = legs
+	-- 8 = feet
+	-- 9 = wrist
+	-- 10 = hands
+	-- 11 = finger 1
+	-- 12 = finger 2
+	-- 13 = trinket 1
+	-- 14 = trinket 2
+	-- 15 = back
+	-- 16 = main hand
+	-- 17 = off hand
+	-- 18 = ranged
+	-- 19 = tabard
+    for i = 1, 19 do
+        if i ~= 4 and i ~= 19 then
+            local itemLink = GetInventoryItemLink(unit, i)
+            if itemLink then
+                local _, _, itemID = string.find(itemLink, "item:(%d+)")
+                local itemLevel = ItemLevel.ilvl_database[tonumber(itemID)] or 0
+                local itemName, _, itemRarity = GetItemInfo(itemID)
 
-  for i=1,19 do
-    if GetInventoryItemLink(target, i) then
-      local _, _, itemID = string.find(GetInventoryItemLink(target, i), "item:(%d+):%d+:%d+:%d+")
-      local _, _, itemLink = string.find(GetInventoryItemLink(target, i), "(item:%d+:%d+:%d+:%d+)");
+                if itemLevel == 0 then              
+                    for realID, engName in pairs(ItemLevel.item_database) do
+                        if engName == itemName then
+                            local newItemLevel = ItemLevel.ilvl_database[realID] or 0
+                            itemLevel = newItemLevel
+                            break
+                        end
+                    end
+                end
 
-      local cilvl = ItemLevel.Database[tonumber(itemID)] or 0
-      local _, _, itemRarity, _, _, _, _, itemSlot, _ = GetItemInfo(itemLink)
-      local r, g, b = .2, .2, .2
+                local coef = slotCoefficients[i] or 1
 
-      if itemRarity and itemSlot then
-        r,g,b, _ = GetItemQualityColor(itemRarity)
-        ar = ar + r ; ag = ag + g ; ab = ab + b
-      end
+                if i == 16 then
+					local _, _, _, _, _, _, _, equipSlot = GetItemInfo(itemID)
+					local itemName = GetItemInfo(itemID)
+					if equipSlot == "INVTYPE_2HWEAPON" then
+                        coef = isHunter and 1 or 2.6836
+                    else
+                        coef = isHunter and 0.5 or 1.6836
+                    end
+                elseif i == 17 then
+                    coef = isHunter and 0.5 or 1
+                elseif i == 18 then
+                    coef = isHunter and 2 or 0.3164
+                end
 
-      count = count + 1
-      if cilvl and cilvl > 0 then
-        ilvl = ilvl + cilvl
-        icount = icount + 1
-      end
+                local itemScore = ItemLevel:Calculate(itemRarity, itemLevel)
+                if itemScore then
+                    ilvl = ilvl + (itemScore * coef)
+                end
+            end
+        end
     end
-  end
 
-  local ar = ItemLevel:round(ar / count, 2);
-  local ag = ItemLevel:round(ag / count, 2);
-  local ab = ItemLevel:round(ab / count, 2);
-  local ilvl = ItemLevel:round(ilvl / icount, 1)
-
-  if ilvl ~= 0 then return ilvl, ar, ag, ab else return nil end
+    local divisor = 17
+    ilvl = tonumber(string.format("%0.1f", (ilvl / divisor) * 1.355))
+  
+    if ilvl ~= 0 then return ilvl else return nil end
 end
 
 function ItemLevel:GetItemLinkByName(name)
-  for itemID = 1, 25818 do
-    local itemName, hyperLink, itemQuality = GetItemInfo(itemID)
-    if (itemName and itemName == name) then
-      local _, _, _, hex = GetItemQualityColor(tonumber(itemQuality))
-      return hex.. "|H"..hyperLink.."|h["..itemName.."]|h|r"
-    end
-  end
+	for itemID = 1, 25818 do
+		local itemName, hyperLink, itemQuality = GetItemInfo(itemID)
+		if (itemName and itemName == name) then
+			local _, _, _, hex = GetItemQualityColor(tonumber(itemQuality))
+			return hex.. "|H"..hyperLink.."|h["..itemName.."]|h|r"
+		end
+	end
 end
 
--- hooks
-local ItemLevelHookSetBagItem = GameTooltip.SetBagItem
+local HookSetBagItem = GameTooltip.SetBagItem
 function GameTooltip.SetBagItem(self, container, slot)
-  GameTooltip.itemLink = GetContainerItemLink(container, slot)
-  _, GameTooltip.itemCount = GetContainerItemInfo(container, slot)
-  return ItemLevelHookSetBagItem(self, container, slot)
+	GameTooltip.itemLink = GetContainerItemLink(container, slot)
+	_, GameTooltip.itemCount = GetContainerItemInfo(container, slot)
+	return HookSetBagItem(self, container, slot)
 end
 
-local ItemLevelHookSetQuestLogItem = GameTooltip.SetQuestLogItem
+local HookSetQuestLogItem = GameTooltip.SetQuestLogItem
 function GameTooltip.SetQuestLogItem(self, itemType, index)
-  GameTooltip.itemLink = GetQuestLogItemLink(itemType, index)
-  if not GameTooltip.itemLink then return end
-  return ItemLevelHookSetQuestLogItem(self, itemType, index)
+	GameTooltip.itemLink = GetQuestLogItemLink(itemType, index)
+	if not GameTooltip.itemLink then return end
+	return HookSetQuestLogItem(self, itemType, index)
 end
 
-local ItemLevelHookSetQuestItem = GameTooltip.SetQuestItem
+local HookSetQuestItem = GameTooltip.SetQuestItem
 function GameTooltip.SetQuestItem(self, itemType, index)
-  GameTooltip.itemLink = GetQuestItemLink(itemType, index)
-  return ItemLevelHookSetQuestItem(self, itemType, index)
+	GameTooltip.itemLink = GetQuestItemLink(itemType, index)
+	return HookSetQuestItem(self, itemType, index)
 end
 
-local ItemLevelHookSetLootItem = GameTooltip.SetLootItem
+local HookSetLootItem = GameTooltip.SetLootItem
 function GameTooltip.SetLootItem(self, slot)
-  GameTooltip.itemLink = GetLootSlotLink(slot)
-  ItemLevelHookSetLootItem(self, slot)
+	GameTooltip.itemLink = GetLootSlotLink(slot)
+	HookSetLootItem(self, slot)
 end
 
-local ItemLevelHookSetInboxItem = GameTooltip.SetInboxItem
+local HookSetInboxItem = GameTooltip.SetInboxItem
 function GameTooltip.SetInboxItem(self, mailID, attachmentIndex)
-  local itemName, itemTexture, inboxItemCount, inboxItemQuality = GetInboxItem(mailID)
-  GameTooltip.itemLink = ItemLevel:GetItemLinkByName(itemName)
-  return ItemLevelHookSetInboxItem(self, mailID, attachmentIndex)
+	local itemName, itemTexture, inboxItemCount, inboxItemQuality = GetInboxItem(mailID)
+	GameTooltip.itemLink = ItemLevel:GetItemLinkByName(itemName)
+	return HookSetInboxItem(self, mailID, attachmentIndex)
 end
 
-local ItemLevelHookSetInventoryItem = GameTooltip.SetInventoryItem
+local HookSetInventoryItem = GameTooltip.SetInventoryItem
 function GameTooltip.SetInventoryItem(self, unit, slot)
-  GameTooltip.itemLink = GetInventoryItemLink(unit, slot)
-  return ItemLevelHookSetInventoryItem(self, unit, slot)
+	GameTooltip.itemLink = GetInventoryItemLink(unit, slot)
+	return HookSetInventoryItem(self, unit, slot)
 end
 
-local ItemLevelHookSetLootRollItem = GameTooltip.SetLootRollItem
+local HookSetLootRollItem = GameTooltip.SetLootRollItem
 function GameTooltip.SetLootRollItem(self, id)
-  GameTooltip.itemLink = GetLootRollItemLink(id)
-  return ItemLevelHookSetLootRollItem(self, id)
+	GameTooltip.itemLink = GetLootRollItemLink(id)
+	return HookSetLootRollItem(self, id)
 end
 
-local ItemLevelHookSetLootRollItem = GameTooltip.SetLootRollItem
-function GameTooltip.SetLootRollItem(self, id)
-  GameTooltip.itemLink = GetLootRollItemLink(id)
-  return ItemLevelHookSetLootRollItem(self, id)
-end
-
-local ItemLevelHookSetMerchantItem = GameTooltip.SetMerchantItem
+local HookSetMerchantItem = GameTooltip.SetMerchantItem
 function GameTooltip.SetMerchantItem(self, merchantIndex)
-  GameTooltip.itemLink = GetMerchantItemLink(merchantIndex)
-  return ItemLevelHookSetMerchantItem(self, merchantIndex)
+	GameTooltip.itemLink = GetMerchantItemLink(merchantIndex)
+	return HookSetMerchantItem(self, merchantIndex)
 end
 
-local ItemLevelHookSetCraftItem = GameTooltip.SetCraftItem
+local HookSetCraftItem = GameTooltip.SetCraftItem
 function GameTooltip.SetCraftItem(self, skill, slot)
-  GameTooltip.itemLink = GetCraftReagentItemLink(skill, slot)
-  return ItemLevelHookSetCraftItem(self, skill, slot)
+	GameTooltip.itemLink = GetCraftReagentItemLink(skill, slot)
+	return HookSetCraftItem(self, skill, slot)
 end
 
-local ItemLevelHookSetCraftSpell = GameTooltip.SetCraftSpell
+local HookSetCraftSpell = GameTooltip.SetCraftSpell
 function GameTooltip.SetCraftSpell(self, slot)
-  GameTooltip.itemLink = GetCraftItemLink(slot)
-  return ItemLevelHookSetCraftSpell(self, slot)
+	GameTooltip.itemLink = GetCraftItemLink(slot)
+	return HookSetCraftSpell(self, slot)
 end
 
-local ItemLevelHookSetTradeSkillItem = GameTooltip.SetTradeSkillItem
+local HookSetTradeSkillItem = GameTooltip.SetTradeSkillItem
 function GameTooltip.SetTradeSkillItem(self, skillIndex, reagentIndex)
-  if reagentIndex then
-    GameTooltip.itemLink = GetTradeSkillReagentItemLink(skillIndex, reagentIndex)
-  else
-    GameTooltip.itemLink = GetTradeSkillItemLink(skillIndex)
-  end
-  return ItemLevelHookSetTradeSkillItem(self, skillIndex, reagentIndex)
+	if reagentIndex then
+		GameTooltip.itemLink = GetTradeSkillReagentItemLink(skillIndex, reagentIndex)
+	else
+		GameTooltip.itemLink = GetTradeSkillItemLink(skillIndex)
+	end
+	return HookSetTradeSkillItem(self, skillIndex, reagentIndex)
 end
 
 local HookSetAuctionItem = GameTooltip.SetAuctionItem
 function GameTooltip.SetAuctionItem(self, atype, index)
   local itemName, _, itemCount = GetAuctionItemInfo(atype, index)
   GameTooltip.itemCount = itemCount
-  GameTooltip.itemLink = GetItemLinkByName(itemName)
+  GameTooltip.itemLink = ItemLevel:GetItemLinkByName(itemName)
   return HookSetAuctionItem(self, atype, index)
 end
 
-local ItemLevelHookSetAuctionSellItem = GameTooltip.SetAuctionSellItem
+local HookSetAuctionSellItem = GameTooltip.SetAuctionSellItem
 function GameTooltip.SetAuctionSellItem(self)
-  local itemName, _, itemCount = GetAuctionSellItemInfo()
-  GameTooltip.itemCount = itemCount
-  GameTooltip.itemLink = ItemLevel:GetItemLinkByName(itemName)
-  return ItemLevelHookSetAuctionSellItem(self)
+	local itemName, _, itemCount = GetAuctionSellItemInfo()
+	GameTooltip.itemCount = itemCount
+	GameTooltip.itemLink = ItemLevel:GetItemLinkByName(itemName)
+	return HookSetAuctionSellItem(self)
 end
 
-local ItemLevelHookSetTradePlayerItem = GameTooltip.SetTradePlayerItem
+local HookSetTradePlayerItem = GameTooltip.SetTradePlayerItem
 function GameTooltip.SetTradePlayerItem(self, index)
-  GameTooltip.itemLink = GetTradePlayerItemLink(index)
-  return ItemLevelHookSetTradePlayerItem(self, index)
+	GameTooltip.itemLink = GetTradePlayerItemLink(index)
+	return HookSetTradePlayerItem(self, index)
 end
 
-local ItemLevelHookSetTradeTargetItem = GameTooltip.SetTradeTargetItem
+local HookSetTradeTargetItem = GameTooltip.SetTradeTargetItem
 function GameTooltip.SetTradeTargetItem(self, index)
-  GameTooltip.itemLink = GetTradeTargetItemLink(index)
-  return ItemLevelHookSetTradeTargetItem(self, index)
+	GameTooltip.itemLink = GetTradeTargetItemLink(index)
+	return HookSetTradeTargetItem(self, index)
 end
-
--- database
-ItemLevel.Database = {}
--- README.md
+++ README.md
@@ -1,12 +1,44 @@
 # ItemLevel
 
-This AddOn is modified from **[Shaguscore](https://github.com/shagu/ShaguScore)** to show item levels instead of item scores.
+This AddOn is modified from **S_ItemTip** (a Chinese addon) to show item levels instead of item scores.
 
 ![character](https://github.com/user-attachments/assets/526b2176-a704-40fc-8aca-244092bb167f)
+
+
+## Update
+- The database has been updated to the lastest version (05/01/2025)
+- The overall item level displayed in the inspection frame or the mouseover tooltip is a weighted average of all important slots. The coefficients are listed below.
+    | Slot | Weight |
+    | ---- | :----: |
+    | head | 1 |
+    | neck | 0.5625 |
+    | shoulder | 0.75 |
+    | chest |  1 |
+    | waist | 0.5625 |
+    | legs | 0.75 |
+    | feet | 0.75 |
+    | wrist | 0.75 |
+    | hands | 0.75 |
+    | finger 1 | 0.5625 |
+    | finger 2 | 0.5625 |
+    | trinket 1 | 0.5625 |
+    | trinket 2 | 0.5625 |
+    | back | 0.5625 |
+    | main hand | 1 |
+    | off hand | 1 |
+    | ranged | 0.3164 |
+
+  Before the calculation, the item levels are scaled based on quality.    
+    | Quality   | Scale |  
+    |-----------|------:|  
+    | Legendary | 1.3   |  
+    | Epic      | 1.0   |  
+    | Rare      | 0.85  |  
+    | Uncommon  | 0.6   |  
+    | Common    | 0.25  |
 
 ## Installation
 
 Option 1: Download the folder and copy it to Wow-Directory\Interface\AddOns
 
 Option 2: Use [GitAddonsManager](https://woblight.gitlab.io/overview/gitaddonsmanager/). Click "+" on the menu and add this repo directly -- https://github.com/qs-crocodyle/itemlevel-turtle.git It is very convenient to keep your addons up to date with GitAddonsManager (simply click the upward arrow on the menu). 
-
